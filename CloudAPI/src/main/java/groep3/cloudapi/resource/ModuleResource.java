9de07b5e4b72678c98a6bd71c8ce6a1edeeff623
Tim <castranoble@gmail.com>
2017-10-09 22:37:10 +0200

    ModuleResource

---
+++ CloudAPI/src/main/java/groep3/cloudapi/resource/ModuleResource.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package groep3.cloudapi.resource;
+
+import groep3.cloudapi.service.ModuleService;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Tim
+ */
+
+@Path( "/Modules" )
+@Consumes ( MediaType.APPLICATION_JSON )
+@Produces ( MediaType.APPLICATION_JSON )
+
+public class ModuleResource extends BaseResource
+{
+    private final ModuleService moduleService;
+    
+    @Inject
+    public ModuleResource (ModuleService moduleSurvice, ModulePresenter modulePresenter)
+    {
+        this.moduleService = moduleService;
+        //this.modulePresenter = modulePresenter;
+    }
+    
+}
