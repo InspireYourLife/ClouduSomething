c2d8d0bc3d234c7c2cfd410d0d88efd4937944f5
Tim <castranoble@gmail.com>
2017-10-10 14:24:34 +0200

    Name changes, call aditions

--- CloudAPI/src/main/java/groep3/cloudapi/resource/ModuleResource.java
+++ CloudAPI/src/main/java/groep3/cloudapi/resource/ModuleResource.java
@@ -8,10 +8,13 @@ package groep3.cloudapi.resource;
 import groep3.cloudapi.model.Module;
 import groep3.cloudapi.presentation.model.ModulePresenter;
 import groep3.cloudapi.service.ModuleService;
+import groep3.cloudapi.service.UserService;
 import java.util.List;
 import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -29,15 +32,17 @@ import javax.ws.rs.core.MediaType;
 public class ModuleResource extends BaseResource
 {
     private final ModuleService moduleService;
+    private final UserService userService;
     
     @Inject
     public ModuleResource (ModuleService moduleSurvice, ModulePresenter modulePresenter)
     {
         this.moduleService = moduleService;
+        this.userService = userService;
         //this.modulePresenter = modulePresenter;
     }
     
-    // Get all modules,should be array? 
+    // Get all modules 
     @GET
     public List<Module> getAll()
     {
@@ -45,5 +50,20 @@ public class ModuleResource extends BaseResource
         return modules;
     }
     
+    //Create a new module
+    @POST
+    public Module create(@Valid Module newModule)
+    {
+            moduleService.create(newModule);
+        return newModule;
+    }
     
+    //Get all modules from specific user
+    @GET
+    public List<Module> getAll()
+    {
+        //How to 
+        List<Module> userModules = moduleService.getAll();
+        return userModules;
+    }
 }
