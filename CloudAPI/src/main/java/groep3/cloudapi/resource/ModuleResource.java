7c6dc710d36418c0fdae263b3e67a3a366a57623
Tim <castranoble@gmail.com>
2017-10-11 15:00:43 +0200

    Typo fixing

--- CloudAPI/src/main/java/groep3/cloudapi/resource/ModuleResource.java
+++ CloudAPI/src/main/java/groep3/cloudapi/resource/ModuleResource.java
@@ -27,21 +27,21 @@ public class ModuleResource extends BaseResource
 {
     private final ModuleService moduleService;
     private final UserService userService;
+    private final ModulePresenter modulePresenter;
     
     @Inject
-    public ModuleResource (ModuleService moduleSurvice, ModulePresenter modulePresenter)
+    public ModuleResource (ModuleService moduleService, UserService userService, ModulePresenter modulePresenter)
     {
-        //this.moduleService = moduleService;
-        //this.userService = userService;
-        //this.modulePresenter = modulePresenter;
+        this.moduleService = moduleService;
+        this.userService = userService;
+        this.modulePresenter = modulePresenter;
     }
     
     // Get all modules 
     @GET
-    @Path ("/modules")
     public List<Module> getAllModules()
     {
-        List<Module> modules = moduleService.getAll();
+        List<Module> modules = moduleService.getAllModules();
         return modules;
     }
     
@@ -50,17 +50,17 @@ public class ModuleResource extends BaseResource
     @Path ("/modules")
     public Module createModule(@Valid Module newModule)
     {
-            moduleService.create(newModule);
+            moduleService.createModule(newModule);
         return newModule;
     }
     
     //Get all modules from specific user
     @GET
     @Path ("/Users/{UserId}/modules")
-    public List<Module> getAllUserModules()
+    public List<Module> getModulesByUserId(String Id)
     {
         //How to incorporate the user id here?
-        List<Module> userModules = moduleService.getAll();
+        List<Module> userModules = moduleService.getModulesByUserId(Id);
         return userModules;
     }
     
