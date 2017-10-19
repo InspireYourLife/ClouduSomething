dede1e76af8bb973afc179f63f8e3cc774d7fc35
Tim <castranoble@gmail.com>
2017-10-19 13:50:46 +0200

    Dependencies

--- CloudAPI/src/main/java/groep3/cloudapi/resource/UserModuleResource.java
+++ CloudAPI/src/main/java/groep3/cloudapi/resource/UserModuleResource.java
@@ -6,12 +6,10 @@ import groep3.cloudapi.service.UserService;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import static jdk.nashorn.internal.runtime.Debug.id;
 
 @Path( "/users" )
 @Consumes ( MediaType.APPLICATION_JSON )
