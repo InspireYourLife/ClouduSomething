a23290d0131797bb351f4a17fd3964b0d9c14ace
Erik van Langen <553322@student.inholland.nl>
2017-10-22 16:54:01 +0200

    Added RolesAllowed to calls + placed comments above calls with simpel function of the call

--- CloudAPI/src/main/java/groep3/cloudapi/resource/UserGoalResource.java
+++ CloudAPI/src/main/java/groep3/cloudapi/resource/UserGoalResource.java
@@ -5,6 +5,7 @@ import groep3.cloudapi.presentation.model.GoalPresenter;
 import groep3.cloudapi.service.GoalService;
 import groep3.cloudapi.service.UserService;
 import java.util.List;
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -33,61 +34,74 @@ public class UserGoalResource extends BaseResource
         this.goalService = goalService;
         this.goalPresenter = goalPresenter;
     }
-    
-    //Get Calls - Goals
+
+    // Returns all goals from a user (all modules)
     @GET
     @Path("/{userId}/goals")
+    @RolesAllowed( {"CLIENT", "CARETAKER"} )
     public List<Goal> getAllGoalsFromUser(@PathParam("userId") String userId) 
     {
         List<Goal> allGoalsFromUser = goalService.getAllGoalsFromUser(userId);
         return allGoalsFromUser;
     }
     
+    // Returns a list of goals from a specific user
     @GET
     @Path("/{userId}/modules/{moduleId}/goals")
+    @RolesAllowed( {"CLIENT", "CARETAKER"} )
     public List<Goal> getGoalsFormModule(@PathParam("userId") String userId, @PathParam("moduleId") String moduleId) 
     {
         List<Goal> goalsFromModule = goalService.getGoalsFromModule(moduleId);
         return goalsFromModule;
     }
     
+    // Returns specific goal form a specific user
     @GET
     @Path("/{userId}/modules/{moduleId}/goals/{goalId}")
+    @RolesAllowed( {"CLIENT", "CARETAKER"} )
     public Goal getGoal(@PathParam("goalId") String goalId)
     {
         Goal goal = goalService.getGoal(goalId);
         return goal;
     }
     
+    // Assigns a goal to a module
     @POST
     @Path("/{userId}/modules/{moduleId}/goals/{goalId}")
+    @RolesAllowed( "CARETAKER" )
     public Boolean assignGoalToModule(@PathParam("userId") String userId, @PathParam("moduleId") String moduleId, @PathParam("goalId") String goalId) 
     {
         boolean hasSucceeded = goalService.assignGoalToModule(moduleId, goalId);
         return hasSucceeded;
     }
-
+    
+    // Remove a goal form a specific user
+    @DELETE
+    @Path("/{userId}/modules/{moduleId}/goals/{goalId}")
+    @RolesAllowed( "CARETAKER" )
+    public Boolean removeGoal(@PathParam("goalId") String goalId)
+    {
+        boolean hasSucceeded = goalService.removeGoal(goalId);
+        return hasSucceeded;
+    }
+    
+    // Changes approval state of a goal
     @PUT
     @Path("/{userId}/modules/{moduleId}/goals/{goalId}/approve")
+    @RolesAllowed( "CARETAKER" )
     public Boolean switchApproveBool(@PathParam("goalId") String goalId)
     {
         boolean hasSucceeded = goalService.switchApproveBool(goalId);
         return hasSucceeded;
     }
     
+    // Changes completion state of a goal
     @PUT
     @Path("/{userId}/modules/{moduleId}/goals/{goalId}/complete")
+    @RolesAllowed( "CARETAKER" )
     public Boolean switchCompleteBool(@PathParam("goalId") String goalId)
     {
         boolean hasSucceeded = goalService.switchCompleteBool(goalId);
-        return hasSucceeded;
-    }
-    
-    @DELETE
-    @Path("/{userId}/modules/{moduleId}/goals/{goalId}")
-    public Boolean removeGoal(@PathParam("goalId") String goalId)
-    {
-        boolean hasSucceeded = goalService.removeGoal(goalId);
         return hasSucceeded;
     }
 }
