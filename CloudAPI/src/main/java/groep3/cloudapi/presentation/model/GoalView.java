07a8487302309168c59d4a1c707bf05581d244cb
Erik van Langen <553322@student.inholland.nl>
2017-10-26 14:07:51 +0200

    Latest version

---
+++ CloudAPI/src/main/java/groep3/cloudapi/presentation/model/GoalView.java
@@ -0,0 +1,12 @@
+package groep3.cloudapi.presentation.model;
+
+public class GoalView
+{
+    public String name;
+    public String description;
+    public String deadline;
+    public String completionDate;
+    public boolean isCompleted;
+    public boolean isApproved;
+    public String creationDate;
+}
