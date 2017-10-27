a7b6dbbe79fce9a3116cdcd0c0ee34528d788938
Jesse van der Mee <jessevandermee@hotmail.com>
2017-10-27 14:58:55 +0200

    swaggerannotaties

--- CloudAPI/src/main/java/groep3/cloudapi/CloudConfiguration.java
+++ CloudAPI/src/main/java/groep3/cloudapi/CloudConfiguration.java
@@ -2,6 +2,7 @@ package groep3.cloudapi;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class CloudConfiguration extends Configuration 
@@ -9,6 +10,9 @@ public class CloudConfiguration extends Configuration
     @NotEmpty
     private String defaultName = "Stranger";
 
+    @JsonProperty
+    public SwaggerBundleConfiguration swagger;
+    
     @JsonProperty
     public String getDefaultName() {
         return defaultName;
